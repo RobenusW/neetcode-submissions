d94f92af8800a366d0a6282a0ece22eae20fa7c2
RobenusW <106640032+RobenusW@users.noreply.github.com>
2026-08-25 13:22:56 -0700

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.go
@@ -0,0 +1,35 @@
+// Edge Cases
+// Only one element in array, return that element
+
+// PseudoCode
+// prefix is strs[0]
+// compare the next element, 
+// cut the prefix to the length of 
+// from left to right with the longest prefix, the letter they diverge the prefix is the index before
+// continue with all the elements
+// return prefix
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func longestCommonPrefix(strs []string) string {
+	longestPrefix := []rune(strs[0])
+	for _, nextWord := range strs[1:] {
+		
+		shorterWordLength := min(len(nextWord), len(longestPrefix))
+		longestPrefix = longestPrefix[:shorterWordLength]
+
+		for i, letter := range nextWord[:shorterWordLength] {
+			if longestPrefix[i] != letter {
+				longestPrefix = longestPrefix[:i]
+				break
+			}
+		}
+
+	}
+	return string(longestPrefix)
+}
